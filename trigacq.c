8a2fd4c8217685147fa1a8772ce03f080244768e
Martijn Jasperse <m.jasperse@gmail.com>
2015-08-23 17:00:33 +0200

    Developed server back-end to include status/keepalive msgs to detect client disconnection on long acquisitions.

--- trigacq.c
+++ trigacq.c
@@ -5,10 +5,14 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #define SIGNAL_LENGTH OSC_FPGA_SIG_LEN
 
 rp_calib_params_t calibr;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+volatile int retries;
 
 int serve(int port); // forward declaration
 
@@ -40,9 +44,9 @@ int process_sig(float *output, int *in_cha_signal, int *in_chb_signal, int dec_f
 
 int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
 {
-
+	int ret = 0;
 	int decim = osc_fpga_cnv_time_range_to_dec(decim_enum);
-	fprintf(stderr,">> %i\n",decim);
+	// fprintf(stderr,">> %i\n",decim);
 	osc_fpga_update_params(0, trig_chan, 0, 
                            0, 0.5, decim_enum,
                            1, 1,
@@ -60,23 +64,33 @@ int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
 	osc_fpga_arm_trigger();
 	osc_fpga_set_trigger(trig);
 	usleep(1);
-	fprintf(stderr,"Waiting for data...  ");
+	fprintf(stderr,"Waiting for data (%is timeout)...  ", timeout);
 	
 	// wait for trigger
-	int retries = timeout*100;	// timeout in s
-	while (retries && !osc_fpga_triggered())
+	pthread_mutex_lock(&mutex);
+	if (retries < 0) ret = -1;
+	retries = timeout*100;	// timeout in s
+	pthread_mutex_unlock(&mutex);
+	
+	while (!ret && !osc_fpga_triggered())
 	{
 		usleep(10000);
-		--retries;
 		if (retries % 100 == 0)
 			fprintf(stderr, "#");
+		pthread_mutex_lock(&mutex);
+		--retries;
+		if (retries <= 0)
+		{
+			fprintf(stderr," >> Timed out! ");
+			ret = -1;
+		}
+		pthread_mutex_unlock(&mutex);
 	}
 	fprintf(stderr,"\n");
 	// waited too long, abort
-	if (retries == 0)
+	if (ret < 0)
 	{
-		fprintf(stderr,"Aborted acquisition.\n");
-		osc_fpga_exit();
+		fprintf(stderr,"Aborted acquisition.  ");
 		return 1;
 	}
 	
@@ -84,6 +98,9 @@ int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
 	int *ptrA, *ptrB;
 	osc_fpga_get_sig_ptr(&ptrA, &ptrB);
 	fprintf(stderr,"Data pointers %p %p\n", ptrA, ptrB);
+	// write size of array
+	int len = SIGNAL_LENGTH;
+	fwrite(&len, sizeof(int), 1, fp);
 	// copy data to local memory
 	float *output = calloc(SIGNAL_LENGTH, 3*sizeof(float));
 	process_sig(output, ptrA, ptrB, osc_fpga_cnv_time_range_to_dec(decim_enum));
@@ -91,7 +108,7 @@ int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
 	fwrite(output,3*sizeof(float),SIGNAL_LENGTH,fp);
 	fprintf(stderr,"Done\n");
 	free(output);
-	return 0;
+	return SIGNAL_LENGTH;
 }
 
 
@@ -133,6 +150,36 @@ int main(int nargs, char** args)
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+
+void* socket_monitor(void* data)
+{
+	int fd = *(int*)data;
+	fprintf(stderr,"Monitor started\n");
+	// try to receive nothing
+	while (1)
+	{
+		int val = -retries;
+		// has acquisition stopped?
+		if (val >= 0) break;
+		// try to send a status message
+		int ref = send(fd, &val, sizeof(val), 0);
+		// did it work?
+		if (ref < 0)
+		{
+			// comms are broken, so abort
+			fprintf(stderr,"\n>> Comms failurs, %i ", errno);
+			break;
+		}
+		// take a nap
+		usleep(100000);	// 100ms
+	}
+	fprintf(stderr,"\n>> Monitor trigger ");
+	pthread_mutex_lock(&mutex);
+	if (retries != 0) retries = -1;
+	pthread_mutex_unlock(&mutex);
+	return NULL;
+}
+
 // based on http://www.cs.ucsb.edu/~almeroth/classes/W01.176B/hw2/examples/tcp-server.c
 int serve(int port)
 {
@@ -140,6 +187,7 @@ int serve(int port)
 	int listenfd;
 	struct sockaddr_in servaddr, cliaddr;
 	char msg[1024];
+	pthread_t monitor;
 	
 	fprintf(stderr,"Starting server\n");
 	listenfd = socket(PF_INET, SOCK_STREAM, 0);
@@ -147,7 +195,11 @@ int serve(int port)
 		fprintf(stderr, "Failed to create socket, %d\n", errno);
 		goto cleanup;
 	}
-	bzero(&servaddr,sizeof(servaddr));
+	// ask kernel to reuse port where possible
+	const int enabled = 1;
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enabled, sizeof(enabled));
+	
+	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(port);
@@ -158,7 +210,7 @@ int serve(int port)
 		goto cleanup;
 	}
 	
-	ret = listen(listenfd, 2);
+	ret = listen(listenfd, 1);
 	if (ret < 0) {
 		fprintf(stderr, "Listen failed, %d\n", errno);
 		goto cleanup;
@@ -167,49 +219,76 @@ int serve(int port)
 	
 	char cliip[INET6_ADDRSTRLEN + 1];
 	FILE *tx;
-	while (1)
+	while (ret >= 0)
 	{
 		ret = 0;
 		socklen_t clilen = sizeof(cliaddr);
+		bzero(&clilen, sizeof(clilen));
 		int connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
 		// http://stackoverflow.com/questions/2064636/getting-the-source-address-of-an-incoming-socket-connection
 		inet_ntop(AF_INET, &cliaddr.sin_addr, cliip, sizeof(cliip));
-		fprintf(stderr,"Accepted connection from '%s'\n",cliip);
+		fprintf(stderr,">> Connected to '%s'\n",cliip);
 		while (ret == 0)
 		{
 			int n = recvfrom(connfd, msg, 1024, 0, (struct sockaddr *)&cliaddr, &clilen);
+			if (n <= 0) break;	// error/disconnection
 			// ensure null-termination (not guaranteed)
+			n = strnlen(msg,n);
+			if (msg[n-1]=='\n')	--n;
+			if (msg[n-1]=='\r') --n;
 			msg[n] = '\0';
+			// blank string?
+			if (n == 0) continue;
+			if (!strcmp(msg,"QUIT"))
+			{
+				fprintf(stderr, "Received QUIT\n");
+				ret = -1;
+				break;
+			}
 			// permit echo message
 			if (!strncmp(msg,"ECHO",4))
 			{
-				fprintf(stderr, "Got 'ECHO'\n");
+				fprintf(stderr, "Replied to ECHO\n");
 				sendto(connfd, msg, n, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
 				continue;
 			}
 			// expect line beginning with ACQ
-			if (strncmp(msg,"ACQ",3))
+			if (!strncmp(msg,"ACQ",3))
 			{
-				fprintf(stderr, "Invalid command '%s'\n", msg);
-				break;
-			}
-			// open an FP
-			tx = fdopen(connfd, "wb");
-			// trap SIGBUS in case client disconnects before data is sent back?
-			if (!tx)
-			{
-				fprintf(stderr, "Failed to get FP for socket\n");
-				break;
-			}
-			// do acquisition
-			{
+				// open an FP
+				tx = fdopen(connfd, "wb");
+				// trap SIGBUS in case client disconnects before data is sent back?
+				if (!tx)
+				{
+					fprintf(stderr, "Failed to get FP for socket\n");
+					break;
+				}
 				int trig_chan = 1;
 				int decim_enum = 0;
 				int timeout = 60;
 				sscanf(msg, "ACQ %i %i %i", &trig_chan, &decim_enum, &timeout);
+				// launch the monitor thread
+				retries = timeout*100;
+				if (pthread_create(&monitor, NULL, &socket_monitor, &connfd))
+				{
+					fprintf(stderr, "Failed to launch monitor thread\n");
+					break;
+				}
+				// do acquisition
 				ret = acquire(tx,trig_chan,decim_enum,timeout);
+				// clean up
+				fclose(tx);
+				pthread_mutex_lock(&mutex);
+				retries = 0;
+				pthread_mutex_unlock(&mutex);
+				// monitor should definitely fire
+				pthread_join(monitor,NULL);
 			}
-			fclose(tx);
+			else
+			{
+				fprintf(stderr, "Invalid command '%s'\n", msg);
+				break;
+			}
 		}
 		fprintf(stderr,"Disconnected\n");
 		close(connfd);
