b4344dc3aed1d6562fb21feab320bc2ca6ba0c91
Martijn Jasperse <m.jasperse@gmail.com>
2015-08-26 15:26:18 +0200

    Server rewrite for better diagnostics, interrupt mechanics

--- trigacq.c
+++ trigacq.c
@@ -15,39 +15,37 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 volatile int retries;
 
 // dirty dirty global variables
-float fs = 125.0e6;
-float *data = NULL;
+#define MASTER_CLOCK 125.0e6
+float fs = MASTER_CLOCK;
+float dt = 1/MASTER_CLOCK;
+float *dataA = NULL;
+float *dataB = NULL;
 
 int serve(int port); // forward declaration
 
 int process_sig(int *in_cha_signal, int *in_chb_signal, int dec_factor)
 {
     float smpl_period = c_osc_fpga_smpl_period * dec_factor;
-    float t_factor = smpl_period * 1e3 /*ms*/;
-    int   in_idx, t_idx, wr_ptr_curr;
-	
-	if (!data)
-		data = calloc(SIGNAL_LENGTH, 3*sizeof(float));
-	
+    dt = smpl_period /*ms*/;
+    
 	// NB: NO TRIGGER ADJUSTMENT
-    osc_fpga_get_wr_ptr(&wr_ptr_curr, &in_idx);
+    int in_idx, t_idx, wr_ptr_curr;
+	osc_fpga_get_wr_ptr(&wr_ptr_curr, &in_idx);
 	fprintf(stderr,"Trigger locations %i %i\n", wr_ptr_curr, in_idx);
     
 	float adc_max_v1 = osc_fpga_calc_adc_max_v(calibr.fe_ch1_fs_g_hi,0);
 	float adc_max_v2 = osc_fpga_calc_adc_max_v(calibr.fe_ch2_fs_g_hi,0);
 	
-	float *ptr = data;
-	for(t_idx=0; t_idx < SIGNAL_LENGTH; ++t_idx, ++in_idx)
+	if (!dataA)	dataA = calloc(SIGNAL_LENGTH, sizeof(float));
+	if (!dataB)	dataB = calloc(SIGNAL_LENGTH, sizeof(float));
+	for (t_idx=0; t_idx < SIGNAL_LENGTH; ++t_idx, ++in_idx)
 	{
         /* Wrap the pointer */
 		if(in_idx >= OSC_FPGA_SIG_LEN) in_idx = in_idx % OSC_FPGA_SIG_LEN;
 		/* Write the output array */
-		ptr[0] = t_idx * t_factor;
-		ptr[1] = osc_fpga_cnv_cnt_to_v(in_cha_signal[in_idx], adc_max_v1, calibr.fe_ch1_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
-		ptr[2] = osc_fpga_cnv_cnt_to_v(in_chb_signal[in_idx], adc_max_v2, calibr.fe_ch2_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
-        ptr += 3;
+		dataA[t_idx] = osc_fpga_cnv_cnt_to_v(in_cha_signal[in_idx], adc_max_v1, calibr.fe_ch1_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
+		dataB[t_idx] = osc_fpga_cnv_cnt_to_v(in_chb_signal[in_idx], adc_max_v2, calibr.fe_ch2_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
 	}
-
     return 0;
 }
 
@@ -63,13 +61,8 @@ int acquire(int trig_chan, int decim_enum, int timeout)
                            0, 0,
                            0, 0,
                            0);
-	fs = 125.0e6 / decim;
+	fs = MASTER_CLOCK / decim;
 	fprintf(stderr,"Decim %u, expected time %.2f ms\n", decim, 1e9/fs);
-	
-	// allocate local arrays
-	int *localA, *localB;
-	localA = calloc(SIGNAL_LENGTH,sizeof(int));
-	localB = calloc(SIGNAL_LENGTH,sizeof(int));
 	
 	// GO!!
 	int trig = osc_fpga_cnv_trig_source(0 /*WAIT*/, trig_chan, 0 /*PGT*/);
@@ -104,28 +97,21 @@ int acquire(int trig_chan, int decim_enum, int timeout)
 	if (ret < 0)
 	{
 		fprintf(stderr,"Aborted acquisition.  ");
-		return 1;
+		return -1;
 	}
 	
 	// get pointer to data
 	int *ptrA, *ptrB;
 	osc_fpga_get_sig_ptr(&ptrA, &ptrB);
 	fprintf(stderr,"Data pointers %p %p\n", ptrA, ptrB);
-	// copy data to local memory
-	// MEMCPY IS A FRIEND THAT HAS BETRAYED US (BUS ERROR?!)
-	// memcpy(localA,ptrA,SIGNAL_LENGTH*sizeof(int));
-	// memcpy(localB,ptrB,SIGNAL_LENGTH*sizeof(int));
 	// transform to analog units
 	fprintf(stderr,"Processing signal\n");
 	process_sig(ptrA, ptrB, osc_fpga_cnv_time_range_to_dec(decim_enum));
-	// binary ejection to fp
-	// fwrite(output,3*sizeof(float),SIGNAL_LENGTH,fp);
 	fprintf(stderr,"Done processing\n");
-	free(localA);
-	free(localB);
 	return SIGNAL_LENGTH;
 }
 
+#include <signal.h>
 
 int main(int nargs, char** args)
 {	
@@ -140,6 +126,9 @@ int main(int nargs, char** args)
 	
 	if ((nargs>1) && ((strcmp(args[1],"-s") == 0)||(strcmp(args[1],"--serve") == 0)))
 	{
+		// I hate my life
+		signal(SIGPIPE, SIG_IGN);
+		// now serve already
 		int port = 5001;
 		if (nargs>2) port = atoi(args[2]);
 		ret = serve(port);
@@ -155,8 +144,15 @@ int main(int nargs, char** args)
 			decim_enum = 0;
 		// acquire the data
 		ret = acquire(trig_chan, decim_enum, 60);
+		float data[3];
 		// write to stdout
-		fwrite(data,SIGNAL_LENGTH,3*sizeof(float),stdout);
+		for (int i = 0; i < SIGNAL_LENGTH; ++i)
+		{
+			data[0] = i * dt;
+			data[1] = dataA[i];
+			data[2] = dataB[i];
+			fwrite(data,3,sizeof(float),stdout);
+		}
 	}
 	osc_fpga_exit();
 	return ret;
@@ -180,9 +176,9 @@ void* socket_monitor(void* pdata)
 		// has acquisition stopped?
 		if (val >= 0) break;
 		// try to send a status message
-		int ref = send(fd, &val, sizeof(val), 0);
+		int ret = send(fd, &val, sizeof(val), 0);
 		// did it work?
-		if (ref < 0)
+		if (ret < 0)
 		{
 			// comms are broken, so abort
 			fprintf(stderr,"\n>> Comms failurs, %i ", errno);
@@ -286,6 +282,8 @@ int serve(int port)
 				}
 				// do acquisition
 				ret = acquire(trig_chan,decim_enum,timeout);
+				if (ret < 0)
+					fprintf(stderr,"Acquisiton failed, %i\n", ret);
 				// clean up
 				pthread_mutex_lock(&mutex);
 				retries = -1;
@@ -296,21 +294,15 @@ int serve(int port)
 				if (ret > 0) 
 				{
 					fprintf(stderr, "Sending data to client\n");
-					int len = SIGNAL_LENGTH;
-					ret = send(fd, &len, sizeof(int), 0);
 					// MILLIONS OF PEACHES, PEACHES FOR ME, MILLIONS OF PEACHES, PEACHES FOR FREE
-					int nb = len * 3*sizeof(float);
-					char *ptr = ( char* )data;
-					while ((nb>0) && (ret>0))
-					{
-						// try to send a large data packet
-						ret = send(fd, ptr, nb, 0);
-						printf("Sent %p, %i, %i\n", ptr, nb, ret);
-						// check how much actually got send, divide it up
-						nb -= ret;
-						ptr += ret;
-					}
-					if (ret < 0) perror("Failed to send data");
+					int len = SIGNAL_LENGTH;
+					ret = send(fd, &len, sizeof(int), 0);	// send # samples
+					ret = send(fd, &fs, sizeof(float), 0);	// send sample rate
+					int nb = len*sizeof(float);
+					ret = send(fd, dataA, nb, 0);
+					if (ret != nb)	perror("Failed to send Channel A");
+					ret = send(fd, dataB, nb, 0);
+					if (ret != nb)	perror("Failed to send Channel B");
 				}
 				// need to reset "ret" now else server will quit
 				ret = 0;
