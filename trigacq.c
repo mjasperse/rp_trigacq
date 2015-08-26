dd2690166d28b5bee62c7ff6b0c9646df03e1bc8
Martijn Jasperse <m.jasperse@gmail.com>
2015-08-26 13:36:12 +0200

    Rewrote server to use send() instead hacked fwrite()

--- trigacq.c
+++ trigacq.c
@@ -14,39 +14,47 @@ rp_calib_params_t calibr;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 volatile int retries;
 
+// dirty dirty global variables
+float fs = 125.0e6;
+float *data = NULL;
+
 int serve(int port); // forward declaration
 
-int process_sig(float *output, int *in_cha_signal, int *in_chb_signal, int dec_factor)
+int process_sig(int *in_cha_signal, int *in_chb_signal, int dec_factor)
 {
     float smpl_period = c_osc_fpga_smpl_period * dec_factor;
     float t_factor = smpl_period * 1e3 /*ms*/;
     int   in_idx, t_idx, wr_ptr_curr;
 	
+	if (!data)
+		data = calloc(SIGNAL_LENGTH, 3*sizeof(float));
+	
 	// NB: NO TRIGGER ADJUSTMENT
     osc_fpga_get_wr_ptr(&wr_ptr_curr, &in_idx);
+	fprintf(stderr,"Trigger locations %i %i\n", wr_ptr_curr, in_idx);
     
 	float adc_max_v1 = osc_fpga_calc_adc_max_v(calibr.fe_ch1_fs_g_hi,0);
 	float adc_max_v2 = osc_fpga_calc_adc_max_v(calibr.fe_ch2_fs_g_hi,0);
 	
+	float *ptr = data;
 	for(t_idx=0; t_idx < SIGNAL_LENGTH; ++t_idx, ++in_idx)
 	{
         /* Wrap the pointer */
 		if(in_idx >= OSC_FPGA_SIG_LEN) in_idx = in_idx % OSC_FPGA_SIG_LEN;
 		/* Write the output array */
-		output[0] = t_idx * t_factor;
-		output[1] = osc_fpga_cnv_cnt_to_v(in_cha_signal[in_idx], adc_max_v1, calibr.fe_ch1_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
-		output[2] = osc_fpga_cnv_cnt_to_v(in_chb_signal[in_idx], adc_max_v2, calibr.fe_ch2_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
-        output += 3;
+		ptr[0] = t_idx * t_factor;
+		ptr[1] = osc_fpga_cnv_cnt_to_v(in_cha_signal[in_idx], adc_max_v1, calibr.fe_ch1_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
+		ptr[2] = osc_fpga_cnv_cnt_to_v(in_chb_signal[in_idx], adc_max_v2, calibr.fe_ch2_dc_offs/*calib_dc_off*/, 0/*user_dc_off*/);
+        ptr += 3;
 	}
 
     return 0;
 }
 
-int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
+int acquire(int trig_chan, int decim_enum, int timeout)
 {
 	int ret = 0;
 	int decim = osc_fpga_cnv_time_range_to_dec(decim_enum);
-	// fprintf(stderr,">> %i\n",decim);
 	osc_fpga_update_params(0, trig_chan, 0, 
                            0, 0.5, decim_enum,
                            1, 1,
@@ -55,8 +63,13 @@ int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
                            0, 0,
                            0, 0,
                            0);
-	float fs = 125.0e6 / decim;
+	fs = 125.0e6 / decim;
 	fprintf(stderr,"Decim %u, expected time %.2f ms\n", decim, 1e9/fs);
+	
+	// allocate local arrays
+	int *localA, *localB;
+	localA = calloc(SIGNAL_LENGTH,sizeof(int));
+	localB = calloc(SIGNAL_LENGTH,sizeof(int));
 	
 	// GO!!
 	int trig = osc_fpga_cnv_trig_source(0 /*WAIT*/, trig_chan, 0 /*PGT*/);
@@ -98,16 +111,18 @@ int acquire(FILE *fp, int trig_chan, int decim_enum, int timeout)
 	int *ptrA, *ptrB;
 	osc_fpga_get_sig_ptr(&ptrA, &ptrB);
 	fprintf(stderr,"Data pointers %p %p\n", ptrA, ptrB);
-	// write size of array
-	int len = SIGNAL_LENGTH;
-	fwrite(&len, sizeof(int), 1, fp);
 	// copy data to local memory
-	float *output = calloc(SIGNAL_LENGTH, 3*sizeof(float));
-	process_sig(output, ptrA, ptrB, osc_fpga_cnv_time_range_to_dec(decim_enum));
+	// MEMCPY IS A FRIEND THAT HAS BETRAYED US (BUS ERROR?!)
+	// memcpy(localA,ptrA,SIGNAL_LENGTH*sizeof(int));
+	// memcpy(localB,ptrB,SIGNAL_LENGTH*sizeof(int));
+	// transform to analog units
+	fprintf(stderr,"Processing signal\n");
+	process_sig(ptrA, ptrB, osc_fpga_cnv_time_range_to_dec(decim_enum));
 	// binary ejection to fp
-	fwrite(output,3*sizeof(float),SIGNAL_LENGTH,fp);
-	fprintf(stderr,"Done\n");
-	free(output);
+	// fwrite(output,3*sizeof(float),SIGNAL_LENGTH,fp);
+	fprintf(stderr,"Done processing\n");
+	free(localA);
+	free(localB);
 	return SIGNAL_LENGTH;
 }
 
@@ -138,8 +153,10 @@ int main(int nargs, char** args)
 			decim_enum = atoi(args[1]);
 		if ((decim_enum<0)||(decim_enum> 5))
 			decim_enum = 0;
-		
-		ret = acquire(stdout,trig_chan,decim_enum, 60);
+		// acquire the data
+		ret = acquire(trig_chan, decim_enum, 60);
+		// write to stdout
+		fwrite(data,SIGNAL_LENGTH,3*sizeof(float),stdout);
 	}
 	osc_fpga_exit();
 	return ret;
@@ -151,13 +168,14 @@ int main(int nargs, char** args)
 #include <arpa/inet.h>
 #include <errno.h>
 
-void* socket_monitor(void* data)
+void* socket_monitor(void* pdata)
 {
-	int fd = *(int*)data;
+	int fd = *(int*)pdata;
 	fprintf(stderr,"Monitor started\n");
 	// try to receive nothing
 	while (1)
 	{
+		pthread_mutex_lock(&mutex);
 		int val = -retries;
 		// has acquisition stopped?
 		if (val >= 0) break;
@@ -170,12 +188,13 @@ void* socket_monitor(void* data)
 			fprintf(stderr,"\n>> Comms failurs, %i ", errno);
 			break;
 		}
+		pthread_mutex_unlock(&mutex);
 		// take a nap
 		usleep(100000);	// 100ms
 	}
-	fprintf(stderr,"\n>> Monitor trigger ");
-	pthread_mutex_lock(&mutex);
-	if (retries != 0) retries = -1;
+	if (retries == 0)
+		fprintf(stderr,"\n>> Monitor trigger ");
+	retries = 0;	// reset (also breaks acquistion loop)
 	pthread_mutex_unlock(&mutex);
 	return NULL;
 }
@@ -218,19 +237,18 @@ int serve(int port)
 	fprintf(stderr,"Listening for connection\n");
 	
 	char cliip[INET6_ADDRSTRLEN + 1];
-	FILE *tx;
 	while (ret >= 0)
 	{
 		ret = 0;
 		socklen_t clilen = sizeof(cliaddr);
 		bzero(&clilen, sizeof(clilen));
-		int connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+		int fd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
 		// http://stackoverflow.com/questions/2064636/getting-the-source-address-of-an-incoming-socket-connection
 		inet_ntop(AF_INET, &cliaddr.sin_addr, cliip, sizeof(cliip));
 		fprintf(stderr,">> Connected to '%s'\n",cliip);
 		while (ret == 0)
 		{
-			int n = recvfrom(connfd, msg, 1024, 0, (struct sockaddr *)&cliaddr, &clilen);
+			int n = recv(fd, msg, 1024, 0);
 			if (n <= 0) break;	// error/disconnection
 			// ensure null-termination (not guaranteed)
 			n = strnlen(msg,n);
@@ -249,40 +267,53 @@ int serve(int port)
 			if (!strncmp(msg,"ECHO",4))
 			{
 				fprintf(stderr, "Replied to ECHO\n");
-				sendto(connfd, msg, n, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+				send(fd, msg, n, 0);
 				continue;
 			}
 			// expect line beginning with ACQ
 			if (!strncmp(msg,"ACQ",3))
 			{
-				// open an FP
-				tx = fdopen(connfd, "wb");
-				// trap SIGBUS in case client disconnects before data is sent back?
-				if (!tx)
-				{
-					fprintf(stderr, "Failed to get FP for socket\n");
-					break;
-				}
 				int trig_chan = 1;
 				int decim_enum = 0;
 				int timeout = 60;
 				sscanf(msg, "ACQ %i %i %i", &trig_chan, &decim_enum, &timeout);
 				// launch the monitor thread
 				retries = timeout*100;
-				if (pthread_create(&monitor, NULL, &socket_monitor, &connfd))
+				if (pthread_create(&monitor, NULL, &socket_monitor, &fd))
 				{
 					fprintf(stderr, "Failed to launch monitor thread\n");
 					break;
 				}
 				// do acquisition
-				ret = acquire(tx,trig_chan,decim_enum,timeout);
+				ret = acquire(trig_chan,decim_enum,timeout);
 				// clean up
-				fclose(tx);
 				pthread_mutex_lock(&mutex);
-				retries = 0;
+				retries = -1;
 				pthread_mutex_unlock(&mutex);
-				// monitor should definitely fire
+				// monitor should definitely fire by now
 				pthread_join(monitor,NULL);
+				// transmit data to client
+				if (ret > 0) 
+				{
+					fprintf(stderr, "Sending data to client\n");
+					int len = SIGNAL_LENGTH;
+					ret = send(fd, &len, sizeof(int), 0);
+					// MILLIONS OF PEACHES, PEACHES FOR ME, MILLIONS OF PEACHES, PEACHES FOR FREE
+					int nb = len * 3*sizeof(float);
+					char *ptr = ( char* )data;
+					while ((nb>0) && (ret>0))
+					{
+						// try to send a large data packet
+						ret = send(fd, ptr, nb, 0);
+						printf("Sent %p, %i, %i\n", ptr, nb, ret);
+						// check how much actually got send, divide it up
+						nb -= ret;
+						ptr += ret;
+					}
+					if (ret < 0) perror("Failed to send data");
+				}
+				// need to reset "ret" now else server will quit
+				ret = 0;
 			}
 			else
 			{
@@ -291,7 +322,7 @@ int serve(int port)
 			}
 		}
 		fprintf(stderr,"Disconnected\n");
-		close(connfd);
+		close(fd);
 	}
 	ret = 0;
 cleanup:
